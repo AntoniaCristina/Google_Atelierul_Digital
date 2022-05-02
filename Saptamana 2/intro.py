01d30ab8df55aa1ff9c7361d8c3a337c557c55f4
AntoniaCristina <antoniacristinasandor@gmail.com>
2022-05-02 21:39:52 +0300

    Saptamana2

--- Saptamana 2/intro.py
+++ Saptamana 2/intro.py
@@ -1,21 +1,56 @@
-#print("Mesaj")
+# print("Mesaj") # instructiune de afisare mesaj
+# variabila_a_2 = 2
+# print(variabila_a_2)
+# mesaj = "\\tAna\"s \"are \\n mere"
+# print(mesaj)
+# numar_mere = 2
+# numar_pere = 3
+# mesaj = f"Ana are {numar_mere} mere" # cea mai utilizata
+# mesaj = "Ana are {} mere si {} pere".format(numar_mere, numar_pere)
+# mesaj = "Ana are " + str(numar_pere) + " mere si " + str(numar_mere) + " pere"
+# print(mesaj)
+# lista = [2, 3, 4, 'Ana']
+# lista.append('2')
+# print(len(lista))
+# print(lista[0:3:2])
+# lista[0] = 'are'
+# print(lista)
+# tuplu = (1, 7, -3, 'a')
+# tuplu = ('1', 2)
+# print(list(tuplu))
+# string_nou = 'Ana are mere'
+# string_nou[1] = 'm'
+# print(string_nou)
+# dictionar = {"cheie1": 1, "cheie2": 2}
+# print(dictionar["cheie3"])
+# print(dictionar.get("cheie3", "3"))
+# dictionar["cheie3"] = "3"
+# dictionar.update({"cheie3": "3"})
+# print(dictionar.items())
+# my_set = {"item1", 1, 2, 3}
+# my_set1 = {2, 3, "item2"}
+# print(my_set1)
+# my_var = 3
+# mesaj = "conditii false"
+# if my_var < 5:
+#     mesaj = "my var este mai mic"
+# elif my_var > 5:
+#     mesaj = "my var este mai mare"
+# elif my_var > -1:
+#     mesaj = "my var este mai mare decat -1"
+# else:
+#     print("conditii false")
+# print(mesaj)
+# variabila_1 = 1
+# while variabila_1 < 3:
+#     print("Set inscriuni")
+#     variabila_1 += 1  # variabila_1 = variabila_1 + 1
+    # break
 
-# while True:
-#         print("Da")
-#         break
-
-# for i in range(10):
-#         print(f'Set instructiuni [{i + 1}]')
-
-# for i in range(5):
-#         if i == 3:
-#                 continue
+# for (i=0; i<ceva; i++) {}
+# cuvant = "Ana are mere"
+# for i, value in enumerate(cuvant):
+#     print(i, value)
+# for i in range(2, 10, 2):
+#     if i == 2:
 #         print(i)
-#
-def my_function(param_1, param_2, *args, **kwargs):
-        print(param_1)
-        print(param_2)
-        print(args)
-        print(kwargs)
-
-my_function(2, 5, d=22, c=7, a=2, b=-3)
